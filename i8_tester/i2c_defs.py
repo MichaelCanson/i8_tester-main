0462e09028ddc0d418b5d5733f81aa1dceabf86a
MichaelCanson <michael@marquis-sys.com>
2023-07-31 15:09:12 -0700

    Test log complete. disable pins after every enable

--- i8_tester/i2c_defs.py
+++ i8_tester/i2c_defs.py
@@ -2,6 +2,7 @@
 Copyright(c) 2023 Marquis Systems Inc.
 
 '''
+from datetime import date
 
 DISABLE_i8 = 0xff
 
@@ -9,10 +10,14 @@ BUS_ADDR = 0x02
 I8_PAIR_ADDR =  [0x20,0x21,0x22,0x23] #PCF address with constant pcf value and LSB: R/W bit set to 1.
 WORD_REG = [[0xff,0xff],[0xff,0xff],[0xff,0xff],[0xff,0xff]] 
 I0_ADDR = [0xfe,0xfd,0xfb,0xf7,0xef,0xdf,0xbf,0x7f]
-ACTIVE_DELAY = 2
 
+ACTIVE_DELAY = 1
+ACTIVE_DURATION = 2
 SEQ = 0
 RAND_PAIRS = 1
+TESTS_TO_RUN =[(0,0),(0,0),(0,0),(0,0)]
 
-TESTS_TO_RUN =[(0,1),(0,0),(0,0),(0,0),\
-               (0,0),(0,0),(0,0),(1,1)]
+FILE_PATH = '/home/kampr/i8_tester-main/i8_tester/logs'
+CSV_PATH = '/home/kampr/i8_tester-main/i8_tester/logs/csv'
+LOG_TITLE = '{}'.format(date.today())
+TEST_LOG = '{}/{}.txt'.format(FILE_PATH,LOG_TITLE)
--- i8_tester/main.py
+++ i8_tester/main.py
@@ -3,13 +3,15 @@ Copyright(c) 2023 Marquis Systems Inc.
 
 '''
 import sys
+import os
 import time
-from i2c_controller import Controller
-import i2c_defs
-
+import csv
+from datetime import  date
 from binascii import hexlify 
 from random import choice, choices, randrange
-
+import logging
+from i2c_controller import Controller
+import i2c_defs
 
 
 def main():
@@ -19,24 +21,48 @@ def main():
         print("number of trials to run: ", sys.argv[2])    
         trials = int(sys.argv[2])
     
-    test_type = i2c_defs.TESTS_TO_RUN[0][0]        
+    #------------ Test Log Configurations -----------------------
+    duplicate_count = 0
+    while os.path.exists(i2c_defs.TEST_LOG):
+        duplicate_count += 1
+
+        i2c_defs.LOG_TITLE = '{}({})'.format(date.today(),duplicate_count)
+        i2c_defs.TEST_LOG = '{}/{}.txt'.format(i2c_defs.FILE_PATH,i2c_defs.LOG_TITLE)
+    try:
+        open(i2c_defs.TEST_LOG,'x')
+    except:
+        duplicate_count = int(i2c_defs.LOG_TITLE[-2]) + 1
+        i2c_defs.LOG_TITLE = '{}({})'.format(date.today(),duplicate_count)
+        i2c_defs.TEST_LOG = '{}/{}.txt'.format(i2c_defs.FILE_PATH,i2c_defs.LOG_TITLE)
+        open(i2c_defs.TEST_LOG,'x')
+
+    logging.basicConfig(filename= i2c_defs.TEST_LOG,filemode='a',level=logging.INFO,\
+         format='%(asctime)s,%(message)s', datefmt='%D %H:%M:%S')
 # ---------------------------------------------------------------
+
+# ----------------- Test Configurations -------------------------
+    test_type = i2c_defs.TESTS_TO_RUN[0][0]
+    loop_count = trials
     while trials:
         print('Trial {} started'.format(trials))
+        curr_count = loop_count - trials
+        i8_count = 0
         for test in range(len(i2c_defs.TESTS_TO_RUN)):
                 print('Test to run ix: {}'.format(test))
-                # for addr_ix in range(len(i2c_defs.I8_PAIR_ADDR)):
-                #     controller.set_pcf_address(addr_ix)
+
+                #set pcf address
                 if test < 4:
                     addr_ix = test
-                    print('addr_ix: {}'.format(addr_ix))
                 else:    
                     addr_ix = (test % 4)
-                    print('addr_ix1: {}'.format(addr_ix))
                 controller.set_pcf_address(addr_ix)
-                for i8_pair_ix in range(2):
-                    test_type = i2c_defs.TESTS_TO_RUN[test][i8_pair_ix]
 
+                for i8_pair_ix in range(2):
+                    i8_count += 1
+                    print('i8_count: {}'.format(i8_count))
+                    test_type = i2c_defs.TESTS_TO_RUN[test][i8_pair_ix]
+                    logging.info('---,---,TEST #{} Trial:{} '
+                    .format(test_type,curr_count))
                     '''
                     Verify which i8 is to be triggered within the ones
                     configured in current pcf address
@@ -50,8 +76,19 @@ def main():
                                 controller.i2c.write(bytearray(controller.word_reg[addr_ix]))
                                 print('PCF ADDR: 0x{0:X} Output: {1} enabled'
                                     .format(i2c_defs.I8_PAIR_ADDR[addr_ix],i))
-                            
+                                    
+                                logging.info('i8.{},input {} ACTIVE,'.format(i8_count, i))
+                                
+                                time.sleep(i2c_defs.ACTIVE_DURATION)
+
+                                controller.word_reg[addr_ix][0] = i2c_defs.DISABLE_i8
+                                controller.word_reg[addr_ix][1] = i2c_defs.DISABLE_i8
+                                controller.i2c.write(bytearray(controller.word_reg[addr_ix]))
+                                logging.info('i8.{},input {} INACTIVE,'.format(i8_count, i))
+
                                 time.sleep(i2c_defs.ACTIVE_DELAY)
+                        
+
                         else:
                             print('\n---- i8.{} Test ----'.format(addr_ix + 2))
                             for i in range(len(i2c_defs.I0_ADDR)):
@@ -61,11 +98,19 @@ def main():
                                 print('PCF ADDR: 0x{0:X} Output: {1} enabled'
                                     .format(i2c_defs.I8_PAIR_ADDR[addr_ix],10 + i))
                                 
+                                logging.info('i8.{},input {} ACTIVE,'.format(i8_count, i))
+                                
+                                time.sleep(i2c_defs.ACTIVE_DURATION)
+
+                                controller.word_reg[addr_ix][0] = i2c_defs.DISABLE_i8
+                                controller.word_reg[addr_ix][1] = i2c_defs.DISABLE_i8
+                                controller.i2c.write(bytearray(controller.word_reg[addr_ix]))
+                                logging.info('i8.{},input {} INACTIVE,'.format(i8_count, i))
+
                                 time.sleep(i2c_defs.ACTIVE_DELAY)
 
                     elif test_type == 1:
-                        print('test type 1 selected successfuly')
-                        time.sleep(1)
+                        print('test type 1: addr_ix: {}'.format(addr_ix))
                 #     i8_addr_1 = (choice(i2c_defs.I8_PAIR_ADDR), randrange(1))
                 #     i8_addr_2 = (choice(i2c_defs.I8_PAIR_ADDR), randrange(1))
                 #     out_addr_1 = choice(i2c_defs.I0_ADDR)
@@ -89,17 +134,26 @@ def main():
 
                 #     active_delay_2 = randrange(1,3)
                 #     active_dur_2 = randrange(3,8)
+                    logging.info('---,---,END OF TEST #{} Trial: {}'
+                        .format(test_type,curr_count))
 
                 #Reset
                 controller.word_reg[addr_ix][0] = i2c_defs.DISABLE_i8
                 controller.word_reg[addr_ix][1] = i2c_defs.DISABLE_i8
                 controller.i2c.write(bytearray([controller.word_reg[addr_ix][0],\
-                                        controller.word_reg[addr_ix][1]]))
-                
-
+                                        controller.word_reg[addr_ix][1]]))       
         trials -= 1
+        #---------------------------------------------------------------
+
+    csv_log = '{}/{}.csv'.format(i2c_defs.CSV_PATH,i2c_defs.LOG_TITLE)
+    with open(i2c_defs.TEST_LOG,'r') as input_file:
+        stripped = (line.strip() for line in input_file)
+        lines = (line.split(",") for line in stripped if line)
+        with open(csv_log, 'w') as output_file:
+            writer = csv.writer(output_file)
+            writer.writerow(('Timestamp','iSTAR i8','Input Port'))
+            writer.writerows(lines)
     print('test trials has been completed.')
-#---------------------------------------------------------------
 
 if __name__ == '__main__':
     sys.exit(main())
